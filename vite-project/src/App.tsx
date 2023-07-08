074574af76ea9d6a87b70bea5814c4c5f5eae149
Aliaksei Rudak <lexarudak@gmail.com>
2023-07-08 16:17:50 +0600

    feat: add vite project

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,17 @@
+import './styles/reset.css'
+import './App.css'
+import { Footer } from './components/footer'
+import { Header } from './components/header'
+import { Main } from './components/main'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
